9df7d5c2677e6646dd50b99659fb10bd3e521c49
Ivan S <i.shiryaev@protonmail.com>
2020-04-26 20:48:20 +0300

    Move code parts to fixture

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/GroupCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/GroupCreationTests.java
@@ -12,16 +12,16 @@ public class GroupCreationTests {
   public void setUp() throws Exception {
     wd = new FirefoxDriver();
     wd.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
-  }
-
-  @Test
-  public void testGroupCreation() throws Exception {
     wd.get("http://192.168.64.2/addressbook/index.php");
     wd.findElement(By.name("user")).click();
     wd.findElement(By.name("user")).clear();
     wd.findElement(By.name("user")).sendKeys("admin");
     wd.findElement(By.name("pass")).clear();
     wd.findElement(By.name("pass")).sendKeys("secret");
+  }
+
+  @Test
+  public void testGroupCreation() throws Exception {
     wd.findElement(By.id("LoginForm")).submit();
     wd.findElement(By.linkText("groups")).click();
     wd.findElement(By.name("new")).click();
